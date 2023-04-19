eae7573ef4c48dd5a85a1c08c15cadc9a09aa1ef
Ileana <ic.chamorro07@gmail.com>
2023-04-19 01:17:48 -0300

    Memorización ( useCallback )

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -2,9 +2,13 @@ import React, { useState } from 'react'
 
 const Contador = () => {
     const [contador, setContador] = useState(0);
+    const [input, setInput] = useState("");
+
     const sumar = () => setContador(contador + 1);
     const restar = () => setContador(contador - 1);
    
+    /*Recibe el evento y actualiza la variable del estado con el valor que traiga el formulario*/
+    const handleInput = (e) => setInput(e.target.value);
   return (
     <div style={{textAlign: 'center'}}>
         <h2>Contador</h2>
@@ -13,6 +17,7 @@ const Contador = () => {
             <button onClick={restar}>-</button>
         </nav>
         <h3>{contador}</h3>
+        <input type="text" onChange={handleInput} value={input}></input>
     </div>
   )
 }
